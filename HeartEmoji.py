2ae0d7226f95a335bc9d4a1e1cd4f150c4c84bed
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2019-09-19 19:03:53 +0530

    heart pattern by emoji

---
+++ HeartEmoji.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[1]:
+
+x="\U0001F619"
+for row in range(6):
+    for coloum in range(7):
+        if (row==0 and coloum %3 !=0) or (row==1 and coloum%3==0) or (row - coloum == 2) or (row+coloum ==8):
+            print(x,end="") #after printing one * control should be in same line so end="" <- null string
+            #by default after one * end of line
+        else:
+            print(" ",end="")
+    print()
+        
+   
+
+
+# In[ ]:
+
+
+
