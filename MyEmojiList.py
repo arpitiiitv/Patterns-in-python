cf3d637b7b96682dac543bf18a8a590e74052db7
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2019-09-19 18:58:24 +0530

    Add files via upload

---
+++ MyEmojiList.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# grinning face 
+print("\U0001f600") 
+
+# grinning squinting face 
+print("\U0001F606") 
+
+# rolling on the floor laughing 
+print("\U0001F923") 
+
+print("\U0001f600")
+# grinning face 
+print("\N{grinning face}") 
+
+# slightly smiling face 
+print("\N{slightly smiling face}") 
+
+# winking face 
+print("\N{winking face}") 
+
+#kissing face
+print("\U0001F619")
+
+#kissing face
+print("\U0001F61A")
+
+
+
+
+
